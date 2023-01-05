13a3fcc758aba1d0de98c3fdc24f380e71105032
Piotr Zaborowski <pzaborowski@ogc.org>
2023-01-06 00:58:50 +0100

    docker working on defs-dev

--- FROST-Server.MQTTP/dockerize.sh
+++ FROST-Server.MQTTP/dockerize.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 docker build -f Dockerfile -t piotrzaborowski/frostplus:2.0.5p --build-arg ARTIFACT_FILE=FROST-Server.MQTTP-2.0.5+.war .
-
+#docker buildx build --platform linux/amd64,linux/arm64,linux/arm/v7 -t piotrzaborowski/frostplus:2.0.5pt --build-arg ARTIFACT_FILE=FROST-Server.MQTTP-2.0.5+.war --push .
 docker push piotrzaborowski/frostplus:2.0.5p
